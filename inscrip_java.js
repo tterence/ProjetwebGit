f0c85c4c4eeb0c1df44c483e04c09504cf49fd5d
youzouuu <yc152470@edu.ece.fr>
2016-04-02 15:21:02 +0100

    Added files via upload

--- inscrip_java.js
+++ inscrip_java.js
@@ -1,6 +1,6 @@
 function verification(x)
 {
-if(x.nom.value == '') // s'il manque le nom
+if(x.nom.value == '') //s'il manque le nom
 {
 alert('Merci de saisir votre nom');
 }
@@ -8,17 +8,21 @@ if(x.prenom.value == '') // s'il manque le prenom
 {
 alert('Merci de saisir votre prénom');
 }
+if(x.pass.value == '') //s'il manque le mot de passe
+{
+alert('Merci de saisir votre mot de passe');
+}
 if(x.pseudo.value == '') // s'il manque le pseudo
 {
-alert('Merci de saisir votre matricule');
+alert('Merci de saisir votre pseudo');
 }
 if(x.mail.value == '') // s'il manque le mail
 {
-alert('Merci de saisir votre numéro de téléphone');
+alert('Merci de saisir votre mail ');
 }
 if(x.adresse.value == '') // s'il manque l'adresse
 {
-alert('Merci de saisir votre numéro de téléphone');
+alert('Merci de saisir votre adresse');
 }
 if(x.numtel.value == '') // s'il manque le tél
 {
@@ -28,9 +32,25 @@ else
 {
 x.go.click();
 }
-VerifMail();
-VerifNum();
+VerifMail(); //permet d'appeler la fonction qui va verifier le mail
+VerifNum(); // permet d'appeler la fonction qui verifier le numero de tel
 }
+
+function verification1(y) //fonction qui va permet la verif pour se onnecter sur la page principal
+{
+if(y.nom.value == '') //s'il manque le nom
+{
+alert('Merci de saisir votre nom');
+}
+if(y.pass.value == '') //s'il manque le mot de passe
+{
+alert('Merci de saisir votre mot de passe');
+}
+else
+{
+x.go.click();
+}
+
 
 function VerifMail()
 	{
@@ -58,4 +78,4 @@ if ( regexTel.test(str))
 
 	alert("Votre numero de telephone n'est pas valide");
 	return regexTel;
-	}
+}
